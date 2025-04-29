bd6a3e2351240da2c9496178228796acc95eac91
valros76 <valerian.dufrene@gmail.com>
2025-04-29 14:47:01 +0200

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { WebSnapListComponent } from './components/web-snap-list/web-snap-list.component';
 
-export const routes: Routes = [];
+const appTitle = "WebSnapApp";
+
+export const routes: Routes = [
+  {
+    path: "",
+    title: `Accueil - ${appTitle}`,
+    loadComponent: () => WebSnapListComponent
+  },
+  {
+    path:"websnaps",
+    title: `Liste des websnap - ${appTitle}`,
+    loadComponent: () => WebSnapListComponent,
+    children:[
+      {
+        path:"websnap/:id",
+        title:`WebSnap - ${appTitle}`,
+        loadComponent: () => WebSnapListComponent
+      }
+    ]
+  },
+  {
+    path:"error/404",
+    title: `Erreur 404 - ${appTitle}`,
+    loadComponent: () => WebSnapListComponent
+  },
+  {
+    path:"**",
+    redirectTo: "erreur/404"
+  }
+];
